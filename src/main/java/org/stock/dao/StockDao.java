d0b2efef8d08869bbf272c4a5736dfc59dd628dc
LSY0108 <seungye1209@gmail.com>
2025-05-27 22:05:42 +0900

    feat: StockDao and  StockVO Upload (#3)

---
+++ src/main/java/org/stock/dao/StockDao.java
@@ -0,0 +1,25 @@
+package org.stock.dao;
+
+import org.stock.domain.StockVO;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface StockDao {
+    // 주식 등록
+    int create(StockVO stock) throws SQLException;
+
+    // 주식 목록 조회
+    List<StockVO> getList() throws SQLException;
+
+    // 주식 정보 조회
+    StockVO get(int id) throws  SQLException;
+
+    // 주식 정보 수정
+    int update(StockVO stock) throws SQLException;
+
+    // 주식 삭제
+    int delete(int id) throws SQLException;
+
+    // close 종료
+    void close() throws SQLException;
+}
